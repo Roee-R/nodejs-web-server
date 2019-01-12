8d8e5c93660fd4663d230231f5587ebb79399bcd
Roee-R <roi890@gmail.com>
2019-01-12 15:24:41 +0200

    Working server with heroku

--- server.js
+++ server.js
@@ -3,22 +3,29 @@ const hbs = require('hbs')
 const fs = require('fs')
 var app = express();
 
+// process.env - is all our envirements verible as key value pers
+// so here we assign the heroku port to the verible (or 3000 defult)
+const port = process.env.PORT || 3000;
 // register the partials path(footer.hbs) to use them in the views(about,index))
-hbs.registerPartials(__dirname+'/views/partials')
-
-// specify the path to serverr (this time the help page)
-app.set('express view engine', 'hbs')
+hbs.registerPartials(__dirname+'/Views/partials')
 
 // specify the source of html views (not so clear)
+
+app.set('view engine', 'hbs')
+app.engine('html', require('hbs').__express);
+
+app.set('views', __dirname+'/Views/');
+// specify the path to serverr (this time the help page)
+
 app.use(express.static(__dirname + '/public')); 
 
 // use when we colling some http request (get) and we can exectue 
 // things before the call is done(next())
 app.use((req,res, next)=>{
     var date = new Date().toString()
-    var log=`${date}: ${req.method} ${req.url}`
+    var log=`${date}: ${req.method} ${req.url} ${process.env.PORT || 3000}`
 
-    fs.appendFile('serverLog', log +'\n',(err)=>{
+    fs.appendFile('serverLog and port', log +'\n',(err)=>{
         if(err){
             console.log(err)
         }
@@ -27,9 +34,9 @@ app.use((req,res, next)=>{
 })
 // because this app.use ise first in the code it execute first
 // so the rest of the pathes will not be loaded
-app.use((req,res,next)=>{
-    res.render('maintenance.hbs')
-})
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs')
+// })
 
 hbs.registerHelper('currentYear',()=>{
     return new Date().getFullYear()
@@ -42,26 +49,29 @@ hbs.registerHelper('toUpperCase',(text)=>{
 
 
 app.get('/',(req,res) =>{
-    res.render('index.hbs', {
+        res.render('index', {
         "welcome": "Wellcome to Roee's site",
-        'pageTitle': 'about page',
+        'pageTitle': 'About page',
         'description': 'On this page you can find all possible informaion you need.'
     })
 })
+app.get('/help1',(req,res) =>{
+    res.render('help.html')
+})
 
 app.get('/about',(req,res)=>{
-res.render('about.hbs',{
-    "welcome": "Wellcome to Roee's site",
-    'pageTitle': 'about page'
+    console.log(`port /about: ${port}`)
+    res.render('about',{
+        "welcome": "Wellcome to Roee's site",
+        'pageTitle': 'About page'
+        })
     })
-})
 
 app.get('/bad',(req,res)=>{
-    res.send({
-        "error": `error your action`
+    res.render('about.hbs',{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is up on port 3000...")
+app.listen(port, ()=>{
+    console.log(`Server is up on port =  ${port}...`)
 })
